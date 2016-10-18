ab8dc1c4c2941efa63393018cbe1f0edf33a2c88
Emil Hjaltason <emil@i-1217c9a8.cs1004cloud.internal>
2016-10-18 22:22:14 +0000

    Welcome Message, test & function

---
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -0,0 +1,9 @@
+
+package is.ru.tictactoe;
+
+public class TicTacToe{
+	public static String welcome(){
+		return ("Welcome to TicTacToe!");
+	}
+}
+
---
+++ src/test/java/is/ru/tictactoe/TicTacToeTest.java
@@ -0,0 +1,16 @@
+
+package is.ru.tictactoe;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TicTacToeTest{
+	
+	@Test
+	public void testWelcome(){
+		TicTacToe t = new TicTacToe();
+		assertEquals("Welcome to TicTacToe!", t.welcome());
+	}
+}
+
