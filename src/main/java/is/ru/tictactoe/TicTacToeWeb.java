a230c2382b33f59606c68b77375133a4287b72c9
hanna15 <hanna15@ru.is>
2016-10-24 20:35:56 +0000

    add port for heroku

--- src/main/java/is/ru/tictactoe/TicTacToeWeb.java
+++ src/main/java/is/ru/tictactoe/TicTacToeWeb.java
@@ -1,10 +1,27 @@
 package is.ru.tictactoe;
 
 import static spark.Spark.*;
+import spark.*;
 
 public class TicTacToeWeb {
 	public static void main(String[] args) {
-		TicTacToe ttt = new TicTacToe();
-		get("/", (req, res) -> ttt.welcome());
+		port(getHerokuAssignedPort());
+		staticFileLocation("/public");
+		
+		SparkApplication tttWeb = new TicTacToeWeb();
+		//get("/", (req, res) -> ttt.welcome());
+		String port = System.getenv("PORT");
+		        if (port != null) {
+				port(Integer.valueOf(port));
+			}
+		      tttWeb.init();
+	}
+
+	@Override
+    public void init() {
+    	final TicTacToe ttt = new TicTacToe();    
+	    post("/welcome", (req, res) -> ttt.welcome());
+	    res.status(200);
+	    return res;
 	}
 }
