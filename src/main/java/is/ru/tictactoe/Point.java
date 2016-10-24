866d85c9d88b487961f17d0ef17b9ff4ef4de77b
swagnus <vanillukrem@gmail.com>
2016-10-24 21:36:49 +0000

    Added Point and pointTest classes

---
+++ src/main/java/is/ru/tictactoe/Point.java
@@ -0,0 +1,19 @@
+package tictactoe;
+
+public class point {
+	private int x;
+	private int y;
+	
+	//default constructor, probably not used tho
+	public point(){
+		x = 0;
+		y = 0;
+	}
+	
+	//another constructor takes x and y as parameters and 
+	//constructs a point
+	public point(int xx, int yy){
+		x = xx;
+		y = yy;
+	}	
+}
---
+++ src/test/java/is/ru/tictactoe/PointTest.java
@@ -0,0 +1,11 @@
+package is.ru.tictactoe;
+
+import static org.junit.Assert.assertEquals;
+import is.ru.tictactoe.Point;
+
+import org.junit.Test;
+
+public class BoardTest{
+	
+	
+}
