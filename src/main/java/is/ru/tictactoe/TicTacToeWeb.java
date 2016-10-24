b61977d32a5ece35003d561900ce53214388c124
hanna15 <hanna15@ru.is>
2016-10-24 12:44:50 +0000

    changing / to /welcome in TicTacToeWeb

--- src/main/java/is/ru/tictactoe/TicTacToeWeb.java
+++ src/main/java/is/ru/tictactoe/TicTacToeWeb.java
@@ -5,6 +5,6 @@ import static spark.Spark.*;
 public class TicTacToeWeb {
 	public static void main(String[] args) {
 		TicTacToe ttt = new TicTacToe();
-		get("/", (req, res) -> ttt.welcome());
+		get("/welcome", (req, res) -> ttt.welcome());
 	}
 }
