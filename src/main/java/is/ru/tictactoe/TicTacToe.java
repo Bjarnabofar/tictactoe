4d1469fa06610d6532cf7255878c7b714f614e8a
hanna15 <hanna15@ru.is>
2016-10-25 00:16:39 +0000

    fix typo in resources

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -1,11 +1,9 @@
-
 package is.ru.tictactoe;
 
 public class TicTacToe{
 	public String welcome(){
 		return ("Welcome to TicTacToe!");
 	}
-
 	//public static void main(String[] args){
 			//System.out.println(welcome());
 	//}
