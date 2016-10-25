19728a9a9498c50060c8518bf3999dec6cb90471
Haukur Halldorsson <haukur7@gmail.com>
2016-10-25 22:52:38 +0000

    add class HumanPlayer + refactoring PlayerTest class

--- src/test/java/is/ru/tictactoe/PlayerTest.java
+++ src/test/java/is/ru/tictactoe/PlayerTest.java
@@ -9,45 +9,45 @@ public class PlayerTest{
 
 	@Test
 	public void testGetWins(){
-		Player p = new Player();
+		Player p = new HumanPlayer();
 		assertEquals(0, p.getWins());
 	}
 
 	@Test
 	public void testGetLosses(){
-		Player p = new Player();
+		Player p = new HumanPlayer();
 		assertEquals(0, p.getLosses());
 	}
 
 	@Test
 	public void testGetDraws(){
-		Player p = new Player();
+		Player p = new HumanPlayer();
 		assertEquals(0, p.getDraws());
 	}
 
 	@Test
 	public void testGetSing(){
-		Player p = new Player();
+		Player p = new HumanPlayer();
 		assertEquals('X', p.getSign());
 	}
 
 	@Test
 	public void testAddWins(){
-		Player p = new Player();
+		Player p = new HumanPlayer();
 		p.addWin();
 		assertEquals(1, p.getWins());
 	}
 
 	@Test
 	public void testAddLosses(){
-		Player p = new Player();
+		Player p = new HumanPlayer();
 		p.addLoss();
 		assertEquals(1, p.getLosses());
 	}
 
 	@Test
 	public void testAddDraw(){
-		Player p = new Player();
+		Player p = new HumanPlayer();
 		p.addDraw();
 		assertEquals(1, p.getDraws());
 	}
