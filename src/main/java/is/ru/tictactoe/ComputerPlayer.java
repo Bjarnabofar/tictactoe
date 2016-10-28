47338fda857349109e70c33a3d4ed21c226d30f5
hanna15 <hanna15@ru.is>
2016-10-28 18:19:47 +0000

    commenting human player class

--- src/main/java/is/ru/tictactoe/ComputerPlayer.java
+++ src/main/java/is/ru/tictactoe/ComputerPlayer.java
@@ -1,12 +1,24 @@
 package is.ru.tictactoe;
 import java.util.concurrent.ThreadLocalRandom;
 
+    /**
+     * @author: Bjarnabofarnir
+     * ComputerPlayer class inherits from Player class, to get 
+     * current move (random) and the sign
+     */
 public class ComputerPlayer extends Player{
 
+	/**
+     * Sets the default sign for computer player as O
+     */
 	public ComputerPlayer () {
 		sign = 'O';
 	}
 
+	/**
+     * generates a random position (x,y) for user
+     * @return: the new Point that the computer will be position on
+     */
 	public Point getMove() {
 
 		int x = ThreadLocalRandom.current().nextInt(3);
