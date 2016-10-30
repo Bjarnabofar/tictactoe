d1f8e3b9eab21a842d8e1ab59de724d125dcb10b
Thorhildur <thorhildurt15@ru.is>
2016-10-30 21:16:35 +0000

    try to fix selenium with travis

--- src/selenium/java/is/ru/tictactoe/SeleniumTestWrapper.java
+++ src/selenium/java/is/ru/tictactoe/SeleniumTestWrapper.java
@@ -3,19 +3,22 @@ package is.ru.tictactoe;
 import java.util.concurrent.TimeUnit;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.openqa.selenium.firefox.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.*;
 
 
 public abstract class SeleniumTestWrapper {
-    static ChromeDriver driver;
-    static String baseUrl = "https://tictactoe-staging.herokuapp.com";
-    static String port;
+   // static ChromeDriver driver;
+    public static WebDriver driver;
+    public static String baseUrl = "https://tictactoe-staging.herokuapp.com";
+    public static String port;
 
     @BeforeClass
     public static void openBrowser(){
         
-        driver = new ChromeDriver();
+        driver = new FirefoxDriver();
+        //driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         /*port = System.getenv("PORT");
