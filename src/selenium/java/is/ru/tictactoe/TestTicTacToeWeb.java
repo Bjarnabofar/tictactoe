63f5371668dd4217f4b2baf9c1a3d4ede6934331
Thorhildur <thorhildurt15@ru.is>
2016-10-30 13:41:59 +0000

    Added selenium test for input as HumanPlayer

---
+++ src/selenium/java/is/ru/tictactoe/TestTicTacToeWeb.java
@@ -0,0 +1,34 @@
+package is.ru.tictactoe;
+
+import org.openqa.selenium.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+//import static org.openqa.selenium.support.ui.ExpectedConditions.*;
+ 
+public class TestTicTacToeWeb extends SeleniumTestWrapper {
+
+    @Test
+    public void titleMatches() throws  InterruptedException {
+        Thread.sleep(5000);
+        driver.get(baseUrl);
+        Thread.sleep(5000);
+        assertEquals("Waiting for some results...", driver.findElement(By.id("results")).getText());
+    }
+
+
+    @Test
+    public void testInputX() {
+    
+    	driver.get(baseUrl);
+    	
+      	WebElement inputXIn01 = driver.findElement(By.id("board_01"));
+      	inputXIn01.click();
+
+      	WebElement inputXIn11 = driver.findElement(By.id("board_11"));
+      	inputXIn11.click();
+
+    }
+
+
+}
