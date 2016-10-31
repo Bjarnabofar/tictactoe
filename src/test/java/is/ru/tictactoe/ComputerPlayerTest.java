1ff0dbd816fce4afec36c67be607fb85d85cc251
swagnus <vanillukrem@gmail.com>
2016-10-31 13:55:15 +0000

    added unit test for computer player

--- src/test/java/is/ru/tictactoe/ComputerPlayerTest.java
+++ src/test/java/is/ru/tictactoe/ComputerPlayerTest.java
@@ -8,32 +8,58 @@ import is.ru.tictactoe.ComputerPlayer;
 
 public class ComputerPlayerTest {
 
+	public class StubNotRandom extends Random{
+        int sequentialNum = 0;
+
+        public StubNotRandom(){
+            super();
+        }
+
+        public int nextInt(){
+            return sequentialNum++;
+        }
+    }
+	
+	public class StubCpPlayer extends Player {
+		public StubCpPlayer getMove() {
+			StubNotRandom rn = new StubNotRandom();
+			
+			int x = rn.nextint();
+			int y = rn.nextint();
+		
+			Point p = new Point(x, y);
+			return p;
+		}
+	}
+	
+	
+
 	@Test
 	public void testGetSing(){
 		Player p = new ComputerPlayer();
 		assertEquals('O', p.getSign());
 	}
+	
 
-	/*@Test
+	@Test
 	public void testRandomPointX() {
-		Player cp = new ComputerPlayer();
+		Player cp = new StubCpPlayer();
 		Point p = cp.getMove();
-		int lo = 0;
-		int hi = 3;
-		int x = p.getX();
-
-		assertTrue(x > lo && x < hi);
+		
+		int xValue = 0;
+		
+		assertEquals(xValue, p.getX());
+		
 	}
 
 	@Test
-	public void testRandomPointY() {
-		Player cp = new ComputerPlayer();
+	public void testRandomPointy() {
+		Player cp = new StubCpPlayer();
 		Point p = cp.getMove();
-
-		int lo = 0;
-		int hi = 3;
-		int y = p.getY();
-
-		assertTrue(y > lo && y < hi);
-	}*/
+		
+		int yValue = 1;
+		
+		assertEquals(yValue, p.getY());
+		
+	}
 }
